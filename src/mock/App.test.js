7201ae4e5762ca017f3ec9c6b4159a5ef67ceb37
박도희 <dpark@aprd.io>
2022-06-24 09:02:28 +0900

    test: add failed jest.fn eg

--- src/mock/App.test.js
+++ src/mock/App.test.js
@@ -13,3 +13,8 @@ test("calls math.subtract", () => {
   app.doSubtract(1, 2);
   expect(math.subtract).toHaveBeenCalledWith(1, 2);
 });
+
+test("calls math.multiply", () => {
+  app.doMultiply(1, 2);
+  expect(math.multiply).toHaveBeenCalledWith(1, 2);
+});
